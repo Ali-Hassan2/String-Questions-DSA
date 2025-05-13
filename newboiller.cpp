c8f88135245df4d2fc154dcdde5cda782caeaeae
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-05-13 21:22:56 +0500

    question 35 adde

---
+++ newboiller.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+    
+
+    int x = 4; cin >> x;
+    cout << "the value is:";
+    return 0;
+}
